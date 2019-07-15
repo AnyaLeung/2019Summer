ca7006b5e2a1d03d7ce2140f23fe747c12d1932a
AnyaLeung <anyaliangww@gmail.com>
2019-07-15 17:15:18 +0800

    Add Tree

---
+++ Tree/94/94.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> res;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==NULL){
+            return res;
+        }
+        else{
+            inorderTraversal(root->left);
+            res.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return res;
+    }
+};
