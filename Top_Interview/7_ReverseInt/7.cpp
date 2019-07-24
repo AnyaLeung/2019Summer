1d79e50a6a3b4a88f48902ba43bf50dd25210f33
AnyaLeung <anyaliangww@gmail.com>
2019-07-24 20:12:48 +0800

    Add 7

---
+++ Top_Interview/7_ReverseInt/7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        
+        while(x){
+            int remainder = x % 10;
+            if(res>INT_MAX/10 || (res==INT_MAX/10 && remainder>7)){
+                return 0;
+            }
+              if(res<INT_MIN/10 || (res==INT_MIN/10 && remainder<-8)){
+                return 0;
+            } 
+            res = res * 10 + remainder;
+                 x /= 10;
+        }
+                 return res;
+    }
+};
