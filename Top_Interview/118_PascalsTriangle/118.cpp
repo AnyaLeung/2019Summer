ad636453f9dfab116aa88c550f5e76e724d30c46
anyaleung <anyaliangww@gmail.com>
2019-08-06 12:35:44 +0800

    Add 118

---
+++ Top_Interview/118_PascalsTriangle/118.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows){
+        vector<vector<int>> res(numRows);
+        if(!numRows){
+            return res;
+        }
+        
+        for(int i=0; i<numRows; i++){
+            for(int j=0; j<=i; j++){
+                if(!j || j==i){
+                    res[i].push_back(1);
+                }
+                else{
+                    res[i].push_back(res[i-1][j-1]+res[i-1][j]);
+                }
+            }
+        }
+        return res;
+    }
+};
