bdfa1d4d047baea3894880b6006d591512ed9853
anyaleung <anyaliangww@gmail.com>
2019-08-08 21:56:10 +0800

    Update 146.cpp

--- Top_Interview/146_LRUCache/146.cpp
+++ Top_Interview/146_LRUCache/146.cpp
@@ -4,7 +4,7 @@ private:
     //double linked list: save {key, val}
     list<pair<int, int>> cache;
     //hash table: key 映射到 (key, val) 在 cache中的位置
-    unordered_map<int, list<pair<int, int>>::iterator> pos; //value is a iterator
+    unordered_map<int, list<pair<int, int>>::iterator> map; //value is a iterator
 
 public:
     LRUCache(int capacity) {
@@ -12,35 +12,35 @@ public:
     }
     
     int get(int key) {
-        auto it = pos.find(key);
-        if(it==pos.end()){ //cannot find
+        auto it = map.find(key);
+        if(it==map.end()){ //cannot find
             return -1;
         }
         else{ //found
             cache.splice(cache.begin(), cache, it->second); //put to top
         }
-        return pos[key]->second;
+        return map[key]->second;
     }
     
     void put(int key, int value) {
-        auto it = pos.find(key);
-        if(it==pos.end()){ //if not found
+        auto it = map.find(key);
+        if(it==map.end()){ //if not found
             if(cache.size()==cap){ //if full
                 auto lastPair = cache.back();
                 int lastKey = lastPair.first;
-                //delete from both pos and cache
-                pos.erase(lastKey);
+                //delete from both map and cache
+                map.erase(lastKey);
                 cache.pop_back();
             }
             //insert into front
             cache.push_front(make_pair(key, value));
-            pos[key] = cache.begin();
+            map[key] = cache.begin();
         }
         else{
             //key exist, move to front
-            cache.erase(pos[key]);
+            cache.erase(map[key]);
             cache.push_front({key, value});
-            pos[key] = cache.begin();
+            map[key] = cache.begin();
         }
     }
 };
