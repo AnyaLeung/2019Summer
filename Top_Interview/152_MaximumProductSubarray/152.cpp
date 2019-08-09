c0490a6f50c8aa1ec6e48384b33f6ea6771ee4da
anyaleung <anyaliangww@gmail.com>
2019-08-09 16:05:52 +0800

    Add 152

---
+++ Top_Interview/152_MaximumProductSubarray/152.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if(nums.empty()){
+            return 0;
+        }
+        int res = INT_MIN;
+        int imax = 1;
+        int imin = 1;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]<0){
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
+            }
+            imax = max(imax*nums[i], nums[i]);
+            imin = min(imin*nums[i], nums[i]);
+            res = max(res, imax);
+        }
+        return res;
+    }
+};
