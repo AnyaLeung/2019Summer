78e1cf4a2b9c0fb48d2b6cfc7b99f1a801d0812c
anyaleung <anyaliangww@gmail.com>
2019-08-31 22:42:00 +0800

    Update 215

---
+++ Top_Interview/215_KthLargestElementInAnArray/215_PQ.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k){
+        priority_queue<int, vector<int>, less<int>> pq;
+        
+        for(auto num : nums){
+            pq.push(num);
+        }
+        
+        for(int i=1; i<k; i++){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
