881f58524574a6fbcd52b91f4166ed644c68df26
anyaleung <anyaliangww@gmail.com>
2019-09-01 12:44:19 +0800

    Update

--- Top_Interview/15_3Sum/15_Easy2Understand.cpp
+++ Top_Interview/15_3Sum/15_Easy2Understand.cpp
@@ -14,25 +14,12 @@ public:
             if(nums[i]>0){
                 continue;
             }
-            //去重
-            if(i && nums[i]==nums[i-1]){
-                continue;
-            }
             int left = i + 1;
             int right = len - 1;
             while(left<right){
                 int sum = nums[i] + nums[left] + nums[right];
-            
                 if(!sum){
                     res.push_back(vector<int>{nums[i], nums[left], nums[right]});
-                    //去重
-                    while(left+1<len && nums[left]==nums[left+1]){
-                        left ++;
-                    }
-                    //去重
-                    while(right-1>0 && nums[right]==nums[right-1]){
-                        right --;
-                    }
                     left ++;
                     right --;
                 }
